c06784ee3cb6dab2bba32c558c7cc6fbc4f396c1
Jeremiah Montoya <jeremiahmontoya@me.com>
2021-09-19 19:53:50 -0700

    add favicons

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,10 +18,31 @@ export default function Home() {
   return (
     <div className="w-full h-screen">
       <Head>
-        <title>bcards</title>
+        <title>Birthday Cards</title>
         <meta name="description" content="bcards" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
 
       <main
@@ -31,7 +52,7 @@ export default function Home() {
           backgroundImage: 'linear-gradient(315deg, #fce043 0%, #fb7ba2 74%)',
         }}
       >
-        <Envelope
+        <Card
           className="bg-purple-300 text-white"
           message={message}
           onBack={
